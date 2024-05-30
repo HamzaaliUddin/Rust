2cf544ab3d127d90838431fb5c9fe3f56306ece8
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-05-30 16:23:07 -0700

    add:Tupple and distracture

--- src/main.rs
+++ src/main.rs
@@ -24,4 +24,15 @@ fn main() {
         // string_dynamic.push_str("I am Hamza Ali");
         // println!("{}",string_dynamic);
 
+    // Tupple
+    let emp_info:(&str,u8) = ("Hamza",21);
+
+        // let emp_name = emp_info.0;
+        // let emp_age = emp_info.1;
+        // println!("Employee Name ={},Employee Age={}",emp_name,emp_age)
+    
+    // Distracture
+    let (employee_name,empoloyee_age)= emp_info;
+    println!("Employee Name ={},Employee Age={}",employee_name,empoloyee_age)
+
 }
