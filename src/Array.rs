7b0069a0a54157e083541248d118abf67776f354
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-05-31 10:28:54 -0700

    add Array

---
+++ src/Array.rs
@@ -0,0 +1,24 @@
+fn main(){
+    // Array  
+        // let  arr:[u8;5] = [1,2,3,4,5]; 
+        // println!("{}",arr[0]);
+
+    // arr={:?}  it can print whole 
+        // let  arr1:[&str;3] = ["hi","hamza","Ali"];
+        // read_arr(arr1);
+        // println!("arr={:?}",arr1);
+
+    // let mut arr1:[&str;3] = ["hi","hamza","Ali"];
+    // write_arr(&mut arr1);
+    // println!("arr1={:?}",arr1);
+}
+
+// fn read_arr(mut arr2:[&str;3]){
+//     arr2[0]= "hello";
+//     println!("arr2 = {:?}",arr2);
+// }
+
+// fn write_arr(arr2:&mut [&str;3]){
+//     println!("arr2={:?}",arr2);
+// }
+
---
+++ src/ScalerType.rs
@@ -0,0 +1,25 @@
+fn main(){
+    // Float Type 
+
+        // let float32_num:f32 = 3.14;// store the number but less than f64
+        // let float64_num:f64= 6.28; // store more number
+        // println!("it is f32:{} it is f64:{}",float32_num,float64_num);
+
+    // Bool Type
+
+        // let is_running:bool = true;
+        // let is_sunny:bool = false;
+
+        // let need_umbrella:bool = is_running && !is_sunny; 
+        // let need_glasses:bool = is_running || is_sunny;
+        // println!("you need {} and you need also {}",need_umbrella,need_glasses)
+
+    // Charater Type
+        
+        // let letter_a:char  ='a'; // ASCII charater
+        // let emoji:char = '🔥'; //unicode emoji   
+        // let kanji:char  = '字'; // unicode charater
+
+        // println!("{} {} {}",letter_a,emoji,kanji)
+}
+
